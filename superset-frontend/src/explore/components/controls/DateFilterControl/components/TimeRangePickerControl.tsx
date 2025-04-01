9371f81c2b3c2d1be467c6208f82b91f8646092b
Alexandru Soare <alexandrusoare@Alexandrus-MacBook-Air.local>
2025-04-01 18:22:09 +0300

    feat(TimeRangePickerControl): adding to the POC

--- superset-frontend/src/explore/components/controls/DateFilterControl/components/TimeRangePickerControl.tsx
+++ superset-frontend/src/explore/components/controls/DateFilterControl/components/TimeRangePickerControl.tsx
@@ -17,27 +17,86 @@
  * under the License.
  */
 
-import { t } from '@superset-ui/core';
+import { styled, t, useTheme } from '@superset-ui/core';
 import { useState } from 'react';
 import { DatePicker, RangePicker } from 'src/components/DatePicker';
+import { FormItem } from 'src/components/Form';
+import Icons from 'src/components/Icons';
+import { Input } from 'src/components/Input';
 import Tabs from 'src/components/Tabs';
+import { Tooltip } from 'src/components/Tooltip';
+
+const StyledFormItem = styled(FormItem)`
+  display: flex;
+  flex-direction: column;
+
+  .ant-form-item-label {
+    text-align: left;
+  }
+`;
 
 function TimeRangePickerControl() {
-  const [show, setShow] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleOpenChange = (open: boolean) => {
+    setIsOpen(true);
+  };
+  const theme = useTheme();
+  const DatePickersWrapper = styled.div`
+    display: flex;
+    width: inherit;
+    justify-content: space-between;
+    padding: 0 ${theme.gridUnit * 5}px;
+  `;
+
+  const StyledPanelWrapper = styled.div`
+    width: 600px;
+  `;
   return (
     <RangePicker
-      panelRender={panelNodel => (
-        <Tabs fullWidth={false} defaultActiveKey="basic">
-          <Tabs.TabPane tab={t('Basic')} key="basic">
-            {panelNodel}
-            <DatePicker />
-            <DatePicker />
-          </Tabs.TabPane>
-          <Tabs.TabPane tab={t('Last')} key="last" />
-          <Tabs.TabPane tab={t('Custom')} key="custom" />
-          <Tabs.TabPane tab={t('Advanced')} key="advanced" />
-        </Tabs>
+      open={isOpen}
+      onOpenChange={handleOpenChange}
+      panelRender={panelNode => (
+        <StyledPanelWrapper>
+          <Tabs fullWidth={false} defaultActiveKey="basic">
+            <Tabs.TabPane tab={t('Basic')} key="basic">
+              {panelNode}
+              <DatePickersWrapper>
+                <StyledFormItem
+                  name="start"
+                  rules={[
+                    {
+                      required: true,
+                      message: t('Title is required'),
+                      whitespace: true,
+                    },
+                  ]}
+                  label={
+                    <>
+                      <span>{t('Start')}</span>
+                      <Tooltip
+                        placement="top"
+                        title={t(
+                          'The start date is inclusive. The end date is exclusive.',
+                        )}
+                      >
+                        <Icons.InfoCircleOutlined />
+                      </Tooltip>
+                    </>
+                  }
+                >
+                  <Input />
+                </StyledFormItem>
+                <FormItem name="end" label={t('End')}>
+                  <DatePicker />
+                </FormItem>
+              </DatePickersWrapper>
+            </Tabs.TabPane>
+            <Tabs.TabPane tab={t('Last')} key="last" />
+            <Tabs.TabPane tab={t('Custom')} key="custom" />
+            <Tabs.TabPane tab={t('Advanced')} key="advanced" />
+          </Tabs>
+        </StyledPanelWrapper>
       )}
     />
   );
