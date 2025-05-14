42717f27f227e386960fbe97a3e3f8d483062e95
Joe Li <joe@preset.io>
2025-05-14 09:57:44 -0700

    setup migration files

---
+++ superset/migrations/versions/2025-05-14_09-51_0808daac6501_metric_currency_should_be_json.py
@@ -0,0 +1,41 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""metric currency should be JSON
+
+Revision ID: 0808daac6501
+Revises: ac6771f3214f
+Create Date: 2025-05-14 09:51:51.656486
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0808daac6501"
+down_revision = "ac6771f3214f"
+
+
+def upgrade():
+    """
+    Convert the currency column to JSON.
+    """
+    pass
+
+
+def downgrade():
+    """
+    Convert the currency column back to text.
+    """
+    pass
